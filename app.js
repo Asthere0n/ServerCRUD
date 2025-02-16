6dc6874911fad413d124c4c59f1f1e44e4f831e4
Asthere0n <cristiansanchezsonido@gmail.com>
2025-02-16 16:28:47 +0000

    Chaged all the titles and Nav bars to use EJS partials

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+import { create } from 'domain';
 import express from 'express';
 import path from 'path';
+import { title } from 'process';
 import { fileURLToPath } from 'url';
 
 // Get the directory name
@@ -10,22 +12,26 @@ const __dirname = path.dirname(__filename)
 // starting the server with express
 const app = express();
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'views'))
 
 // listen for requests
 app.listen(3000);
 console.log(`Server functioning in http://localhost:3000`)
 
 app.get('/', (req, res)=>{
-    res.render('index')
+    res.render('index', {title: 'Home'})
 })
 
-// Redirection 301
-app.get('/index.html', (req, res)=>{
-    res.redirect('/')
+app.get('/blogs', (req, res)=>{
+    res.render('blogs', {title: 'Blogs'})
+})
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new Post'})
 })
 
 app.get('/about', (req, res)=>{
-    res.render('about')
+    res.render('about', {title : 'About Us'})
 })
 
 // 404 page
